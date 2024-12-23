08992fd1f241046cddf44c7f0fa1d44015f7443e
ShariqueAlly <shariqueali4765@gmail.com>
2024-12-23 15:56:59 +0530

    first commit

---
+++ prastik.js
@@ -0,0 +1,11 @@
+const divTag = document.getElementById('div1');
+
+divTag.style.width='300px';
+divTag.style.height='200px';
+divTag.style.backgroundColor='yellow';
+divTag.innerText="Hellow, I'm a div!";
+divTag.style.color='blue';
+divTag.style.fontSize='18px';
+divTag.style.fontWeight='bold';
+divTag.style.padding='10px';
+divTag.style.margin ='20px';
